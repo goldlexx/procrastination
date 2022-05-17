3d5b83004004373253f93cf9e25d98e9802e254e
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-05-17 07:35:29 +0300

    Магия присваивания

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let time=34;
+let time = 34;
 console.log('Старт поездки. Осталось минут:' + time);
+time = time - 15;
+console.log('Немного сториз в инстаграме. Осталось минут:' + time);
+time = time - 10;
+console.log('Немного новостей на meduza.io. Осталось минут:' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Томск');
