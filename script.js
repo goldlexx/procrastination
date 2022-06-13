fb2a423288c0606808592793b5253a785188fc44
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-06-13 08:36:17 +0300

    Реакция на действия пользователя

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ let phrases = [
   'разобраться, о чём поют рэперы',
   'Юрий Дудь',
   'расставить книги на полке по цвету',
-  'читать про зарплаты в Сан-Франциско',
+  'читать про зарплаты в Сан-Франциско'
 ];
 
 function getRandomElement(arr) {
   let randIndex = Math.floor(Math.random() * arr.length);
   return arr[randIndex];
 }
+
 let button = document.querySelector('.button');
-let advice = document.querySelector('.advice');
 let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
 let image = document.querySelector('.image');
-phrase.textContent = getRandomElement(phrases);
+
+button.addEventListener('click', function () {
+phrase.textContent=getRandomElement(phrases)
+})
