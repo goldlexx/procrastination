7a7e698cf7d21deedbfd03b457b727a3e85ada7e
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-06-20 09:32:19 +0300

    feat: add phrase watch

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 let phrases = [
-  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
-  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
-  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
-  { text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
-  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
-  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
-  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
-  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
-  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
-{ text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+  {
+    text: 'отправить другу смешную гифку',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif',
+  },
+  {
+    text: 'посмотреть скидки на авиабилеты',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png',
+  },
+  {
+    text: 'разобраться, о чём поют рэперы',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png',
+  },
+  {
+    text: 'Юрий Дудь',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png',
+  },
+  {
+    text: 'расставить книги на полке по цвету',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png',
+  },
+  {
+    text: 'читать про зарплаты в Сан-Франциско',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png',
+  },
+  {
+    text: 'прочитать новости и ужаснуться в комментариях',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png',
+  },
+  {
+    text: 'попасть в поток грустных песен и вспомнить все ошибки молодости',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png',
+  },
+  {
+    text: 'посмотреть трейлер сериала и заодно первый сезон',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png',
+  },
+  {
+    text: 'проверить непрочитанное в Telegram-каналах',
+    image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png',
+  },
 ];
+
 function getRandomElement(arr) {
   let randIndex = Math.floor(Math.random() * arr.length);
   return arr[randIndex];
@@ -22,7 +53,7 @@ let image = document.querySelector('.image');
 
 button.addEventListener('click', function () {
   let randomElement = getRandomElement(phrases);
-  smoothly(phrase, 'textContent', randomElement.text) ;
+  smoothly(phrase, 'textContent', randomElement.text);
   smoothly(image, 'src', randomElement.image);
 
   if (randomElement.text.length > 40) {
@@ -31,3 +62,7 @@ button.addEventListener('click', function () {
     advice.style.fontSize = '42px';
   }
 });
+for (let i = 0; i <= 2; i = i + 1) {
+  smoothly(phrase, 'textContent', phrases[i].text);
+  smoothly(image, 'src', phrases[i].image);
+}
